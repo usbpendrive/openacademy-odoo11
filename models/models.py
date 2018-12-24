4f69f66d4dbcdb9e409509b2e855c9ab6629a9ba
usbpendrive <ferry.fernando@gmail.com>
2018-12-24 08:36:07 +0700

    Computed value and default value.

--- models/models.py
+++ models/models.py
@@ -17,9 +17,10 @@ class Session(models.Model):
     _name = 'openacademy.session'
 
     name = fields.Char(required=True)
-    start_date = fields.Date()
+    start_date = fields.Date(default=fields.Date.today)
     duration = fields.Float(digits=(6, 2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
+    active = fields.Boolean(default=True)
 
     instructor_id = fields.Many2one('res.partner', string="Instructor",
                                     domain=['|', ('instructor', '=', True),
@@ -27,3 +28,13 @@ class Session(models.Model):
     course_id = fields.Many2one("openacademy.course", ondelete='cascade',
                                 string="Course", required=True)
     attendee_ids = fields.Many2many("res.partner", string="Attendees")
+
+    taken_seats = fields.Float(string="Taken seats", compute='_taken_seats')
+
+    @api.depends('seats', 'attendee_ids')
+    def _taken_seats(self):
+        for r in self:
+            if not r.seats:
+                r.taken_seats = 0.0
+            else:
+                r.taken_seats = 100.0 * len(r.attendee_ids) / r.seats
