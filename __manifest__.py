2e5829478a118f5519a5ae875e5cc0d373142add
usbpendrive <ferry.fernando@gmail.com>
2018-12-24 08:00:37 +0700

    New line fix (PEP8). Base simple model (with demo data)

--- __manifest__.py
+++ __manifest__.py
@@ -1,23 +1,24 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "openacademy",
+    'name': "Open Academy",
 
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+    'summary': """Manage trainings""",
 
     'description': """
-        Long description of module's purpose
+        Open Academy module for managing trainings:
+            - training course
+            - training session
+            - attendees registration
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Fernando",
+    'website': "https://github.com/usbpendrive/openacademy-odoo11",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/11.0/odoo/addons/base/module/module_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
+    'category': 'Test',
+    'version': '11.0.1.0.0',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
--- models/models.py
+++ models/models.py
@@ -1,15 +1,8 @@
-# -*- coding: utf-8 -*-
+from odoo import api, models, fields
 
-from odoo import models, fields, api
 
-# class openacademy(models.Model):
-#     _name = 'openacademy.openacademy'
+class Course(models.Model):
+    _name = 'openacademy.course'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+    name = fields.Char(string="Title", required=True)
+    description = fields.Text()
