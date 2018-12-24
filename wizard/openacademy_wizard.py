3aa00999ded8fea1ab98401c1d7494e2625fc97b
usbpendrive <ferry.fernando@gmail.com>
2018-12-24 09:43:48 +0700

    Openacademy wizard form.

---
+++ wizard/openacademy_wizard.py
@@ -0,0 +1,18 @@
+from odoo import api, fields, models
+
+
+class OpenacademyWizard(models.Model):
+    _name = 'openacademy.wizard'
+
+    def _default_sessions(self):
+        return self.env['openacademy.session'].browse(self._context_get('active_ids'))
+
+    session_ids = fields.Many2many('openacademy.session', string="Sessions",
+                                 required=True, default=_default_sessions)
+    attendee_ids = fields.Many2many('res.partner', string="Attendees")
+
+    @api.multi
+    def subscribe(self):
+        for session in self.session_id:
+            session.attendee_ids |= self.attendee_ids
+        return {}
