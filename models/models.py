6308c83836b0924f0c7692a9b887c54fa8f64866
usbpendrive <ferry.fernando@gmail.com>
2018-12-24 09:16:22 +0700

    Session kanban view.

--- models/models.py
+++ models/models.py
@@ -46,6 +46,7 @@ class Session(models.Model):
     duration = fields.Float(digits=(6, 2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
     active = fields.Boolean(default=True)
+    color = fields.Integer()
 
     instructor_id = fields.Many2one('res.partner', string="Instructor",
                                     domain=['|', ('instructor', '=', True),
