220fb44091f5257409750cbd1be0e3e26d2b2a26
usbpendrive <ferry.fernando@gmail.com>
2018-12-24 08:08:16 +0700

    Menu items and action window.

--- __manifest__.py
+++ __manifest__.py
@@ -25,9 +25,7 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'views/openacademy.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
