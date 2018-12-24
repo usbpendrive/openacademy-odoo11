15f6eaf6ce665465569bc59d49a6047e17ebe5c4
usbpendrive <ferry.fernando@gmail.com>
2018-12-24 08:50:11 +0700

    Session calendar view.

--- models/models.py
+++ models/models.py
@@ -1,3 +1,5 @@
+from datetime import timedelta
+
 from odoo import api, models, fields, exceptions
 
 
@@ -53,6 +55,8 @@ class Session(models.Model):
     attendee_ids = fields.Many2many("res.partner", string="Attendees")
 
     taken_seats = fields.Float(string="Taken seats", compute='_taken_seats')
+    end_date = fields.Date(string="End Date", store=True,
+                           compute='_get_end_date', inverse='_set_end_date')
 
     @api.depends('seats', 'attendee_ids')
     def _taken_seats(self):
@@ -78,6 +82,26 @@ class Session(models.Model):
                     "message": "Increase seats or remove excess attendees",
                 },
             }
+
+    @api.depends('start_date', 'duration')
+    def _get_end_date(self):
+        for r in self:
+            if not (r.start_date and r.duration):
+                r.end_date = r.start_date
+                continue
+
+            start = fields.Datetime.from_string(r.start_date)
+            duration = timedelta(days=r.duration, seconds=1)
+            r.end_date = start + duration
+
+    def set_end_date(self):
+        for r in self:
+            if not (r.start_date and r.end_date):
+                continue
+
+            start_date = fields.Datetime.from_string(r.start_date)
+            end_date = fields.Datetime.from_string(r.end_date)
+            r.duration = (end_date - start_date).days + 1
 
     @api.constrains('instructor_id', 'attendee_ids')
     def _check_instructor_not_in_attendees(self):
