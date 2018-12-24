7b46c4b34117a5a7be6638977df918f645789afc
usbpendrive <ferry.fernando@gmail.com>
2018-12-24 08:41:16 +0700

    Model and SQL constrains.

--- models/models.py
+++ models/models.py
@@ -1,4 +1,4 @@
-from odoo import api, models, fields
+from odoo import api, models, fields, exceptions
 
 
 class Course(models.Model):
@@ -11,6 +11,15 @@ class Course(models.Model):
                                      ondelete='set null', string="Responsible", index=True)
     session_ids = fields.One2many(
         "openacademy.session", 'course_id', string="Sessions")
+
+    _sql_constraints = [
+        ("name_description_check",
+         'CHECK(name != description)',
+         "The title of the course should not be the description"),
+        ("name_unique",
+         'UNIQUE(name)',
+         "The course title must be unique"),
+    ]
 
 
 class Session(models.Model):
@@ -38,3 +47,26 @@ class Session(models.Model):
                 r.taken_seats = 0.0
             else:
                 r.taken_seats = 100.0 * len(r.attendee_ids) / r.seats
+
+    @api.onchange('seats', 'attendee_ids')
+    def _verify_valid_seats(self):
+        if self.seats < 0:
+            return {
+                "warning": {
+                    "title": "Incorrect 'seats' value",
+                    "message": "The number of available seats may not be negative",
+                },
+            }
+        if self.seats < len(self.attendee_ids):
+            return {
+                "warning": {
+                    "title": "Too many attendees",
+                    "message": "Increase seats or remove excess attendees",
+                },
+            }
+
+    @api.constrains('instructor_id', 'attendee_ids')
+    def _check_instructor_not_in_attendees(self):
+        for r in self:
+            if r.instructor_id and r.instructor_id in r.attendee_ids:
+                raise exceptions.ValidationError("A session's instructor can't be an attendee")
